4bcc64267e3ed7dc247202927148ac36a95c2aa0
jahseyirving <seye9000@gmail.com>
2024-05-04 16:00:15 +0000

    ajout des modification

---
+++ js/main.js
@@ -0,0 +1,19 @@
+alert("Hello there random user! :")
+var visitorName = prompt("What's your name : ")
+var isCoolWithIt = confirm("Are you ok with it?")
+var variable = "content";
+console.log(visitorName)
+
+// I’m a single line comment
+/*
+And I’m a multi-lines comment,
+Anything in here will be ignored by the Javascript
+Interpreter.
+var b = 3 + 3
+*/
+var person1 = {
+	name: "foulan",
+	age: 9000,
+	isStudent: true
+}
+console.log(person1)
